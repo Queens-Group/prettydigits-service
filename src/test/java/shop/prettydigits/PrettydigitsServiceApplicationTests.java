638fb43a49de62e03d53bb8dba9c62db378f5df3
hakimamarullah <hakimamarullah@gmail.com>
2024-06-21 17:31:38 +0700

    Initial commit

---
+++ src/test/java/shop/prettydigits/PrettydigitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.prettydigits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrettydigitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
